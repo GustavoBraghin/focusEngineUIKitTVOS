cd0222d52d8eee6373b1d18c50236ee221389514
GustavoBraghin <gugabraghin@gmail.com>
2022-05-11 17:38:44 -0300

    testing focus delegates

--- focusEngine/ViewController.swift
+++ focusEngine/ViewController.swift
@@ -11,9 +11,38 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view
+    }
+    
+    override func shouldUpdateFocus(in context: UIFocusUpdateContext) -> Bool {
+        if context.nextFocusedView == leftTop || context.nextFocusedView == leftBot {
+            rightView.backgroundColor = .red
+            leftView.backgroundColor = .clear
+        }else{
+            rightView.backgroundColor = .clear
+            leftView.backgroundColor = .red
+        }
+        return true
+    }
+    
+    override func didUpdateFocus(in context: UIFocusUpdateContext, with coordinator: UIFocusAnimationCoordinator) {
+        super.didUpdateFocus(in: context, with: coordinator)
+        context.previouslyFocusedView?.backgroundColor = .blue
+        context.nextFocusedView?.backgroundColor = .yellow
     }
 
+    @IBOutlet var leftTop: UIButton!
+    @IBOutlet var leftBot: UIButton!
+    @IBOutlet var rightTop: UIButton!
+    @IBOutlet var rightBot: UIButton!
+    
+    @IBOutlet var leftView: UIView!
+    @IBOutlet var rightView: UIView!
+}
 
+extension UIButton {
+    open override var canBecomeFocused: Bool{
+        return false
+    }
 }
 
